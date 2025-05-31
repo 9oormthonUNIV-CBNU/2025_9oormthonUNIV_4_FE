b2bcf23429a7b0fd2ebf2b5c55768fc01065c9a1
iconoclast7 <jinwoo901901@gmail.com>
2025-06-01 01:28:06 +0900

    navbar 변경

--- mustep-fe/src/components/Navbar.jsx
+++ mustep-fe/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { Link, useNavigate } from "react-router";
 import Logo from "../assets/logo.png";
+import User from "../assets/user_icon.svg";
 
 const NavbarContainer = styled.div`
   background-color: #f0f3f5;
@@ -22,15 +23,39 @@ const NavItemGroup = styled.div`
 const BtnGroup = styled.div`
   margin-left: auto;
   display: flex;
-  gap: 12px;
+  gap: 8px;
 `;
 
 const AuthBtn = styled.button`
-  padding: 7px 7px 7px 7px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+  padding: 8px 16px;
+  border-radius: 8px;
+  height: 36px;
+  font-size: 16px;
+  font-weight: bold;
+  background-color: #DDE0E6;
+  border: none;
+  cursor: pointer;
+`;
+
+const UserImg = styled.img`
+  width: 20px;
+  height: 20px;
 `;
 
 const LogoImg = styled.img`
   width: 200px;
+`;
+
+const LogoText = styled.span`
+  font-size: 18px;
+  font-weight: bold;
+  margin-left: 61px;
+  display: flex;
+  align-items: center;
 `;
 
 const Navbar = () => {
@@ -43,11 +68,15 @@ const Navbar = () => {
           <LogoImg src={Logo} alt="logo" />
         </LogoGroup>
       </Link>
+          
+          <LogoText>기업 프로젝트</LogoText>
 
       <NavItemGroup></NavItemGroup>
       <BtnGroup>
-        <AuthBtn onClick={() => navigate("/login")}>Login</AuthBtn>
-        <AuthBtn>Join</AuthBtn>
+        <AuthBtn onClick={() => navigate()}>
+          <UserImg src={User} alt="user_icon" />
+           이름이름님
+        </AuthBtn>
       </BtnGroup>
     </NavbarContainer>
   );
